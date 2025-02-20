c6c01154402fd7a7f06f5894c5e7e722434686e9
pranavbirajdar1 <pranavbi55@gmail.com>
2025-02-20 11:46:42 +0530

    deactivation and deletion something

--- src/settings/urls.py
+++ src/settings/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
      path('settings/update/<int:id>/',updated , name="profile_up"),
     path('settings/add-update/<int:id>/',addupdated , name="address_up"),
     path('settings/con-update/<int:id>/',conupdated , name="contact_up"),
+    path('settings/acc-deactivate/<int:id>/',deactivate_acc , name="deactivation"),
+    path('settings/acc-deletion/<int:id>/',delete_account , name="deletion"),
    
    
     
