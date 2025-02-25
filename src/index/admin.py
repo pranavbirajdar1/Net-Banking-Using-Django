663281b38a7bd9331ca27d142040010013d5f2df
pranavbirajdar1 <pranavbi55@gmail.com>
2025-02-25 11:33:52 +0530

    Fixed bugs and removed unused files

--- src/index/admin.py
+++ src/index/admin.py
@@ -2,13 +2,8 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-@admin.register(CustomerPersonalInfo)
-@admin.register(Contact)
-@admin.register(AddressInfo)
-@admin.register(AccountDetails)
-@admin.register(SecurityQuestion)
-@admin.register(IsAuthenticated)
-class CustomerPersonalInfoAdmin(admin.ModelAdmin):
-    
-    
-    pass
+admin.site.register(CustomerPersonalInfo)
+admin.site.register(Contact)
+admin.site.register(AddressInfo)
+admin.site.register(AccountDetails)
+admin.site.register(SecurityQuestion)
