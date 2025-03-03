8f9b69bbff715b76a3f81da96cd2b3f5d15417b2
pranavbirajdar1 <pranavbi55@gmail.com>
2025-03-03 13:32:53 +0530

    regi folder and urls  for the forgot password functionalit

--- src/index/urls.py
+++ src/index/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from .views import *
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('',index , name="home"),
     
@@ -8,5 +10,18 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
     
     
-    
+    #Django password reset links
+    path('password_reset/', 
+         auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'), 
+         name='password_reset'),
+    path('password_reset/done/', 
+         auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), 
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', 
+         auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), 
+         name='password_reset_confirm'),
+    path('reset/done/', 
+         auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), 
+         name='password_reset_complete'),
+
 ]
