4b1e66bd969382cd697bf00b7dd699ddb9089421
pranavbirajdar1 <pranavbi55@gmail.com>
2025-03-04 10:39:16 +0530

    User profile pic  management working

--- src/userProfile/models.py
+++ src/userProfile/models.py
@@ -3,7 +3,12 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile',related_query_name='profile',db_index=True) # Delete profile when user is deleted
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics',null=True,blank=True,verbose_name='user_profile')
 
+    def __str__(self):
+        return f'{self.user.username} Profile' #show how we want it to be displayed
 
 
 
