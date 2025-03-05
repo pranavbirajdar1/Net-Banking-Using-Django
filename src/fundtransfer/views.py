02609adf007a4d1d2360a65fb5c736744b15baea
pranavbirajdar1 <pranavbi55@gmail.com>
2025-03-05 12:02:47 +0530

    fund transfer with help of method from the Transaction model

--- src/fundtransfer/views.py
+++ src/fundtransfer/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render , redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
+from .models import  Transaction
 from decimal import Decimal,InvalidOperation
 import logging
-from .services import fund_transferrs
 # Create your views here.
-logger = logging.getLogger(__name__)
+
 
 @login_required
 def fundtransfer(request):
@@ -16,16 +16,13 @@ def fundtransfer(request):
         amounts = request.POST.get('amount')
         sender = request.user
         receiver_username = receiver_usernames.strip()
-        logger.info(f'Received Transfer Request : Receiver ={receiver_username} , Amount = {amounts}')
         if not amounts:
             messages.error(request, 'Please enter the Amount')
             return redirect('fundtransfer')
         try:
             receiver = User.objects.get(username__iexact=receiver_username)
-            logger.info(f'Found Receiver : {receiver_username}')
         except User.DoesNotExist:
             messages.error(request, 'Receiver not found , checkname')
-            logger.error(f'Receiver Not Found : {receiver_username}')
             return redirect('fundtransfer')
        
         #Validate the amt must be +ve Number
@@ -55,14 +52,12 @@ def fundtransfer(request):
         
         # Perform the Transfer
         try:
-            fund_transferrs.delay(sender=sender,receiver=receiver,amount=amounts)
+            Transaction.transfer_funds(sender=sender,receiver=receiver,amount=amounts)
             messages.success(request, f'Successfully Transferred {amounts} to {receiver_username.username}.')
-            logger.info( f'Successfully Transferred {amounts} to {receiver_username.username}.')
             return redirect('fundtransfer')
             
         except Exception as e:
             messages.error(request, f'Error occurred during transfer {str(e)}')
-            logger.info(f'Transfer Request sent to Celery : ${amounts} from {request.user.uesrname} to {receiver.username}')
             return redirect('fundtransfer')
     # Fetch users excluding logged in user  (for dropdown option in template)
     users = User.objects.exclude(username=user.username).exclude(is_superuser=True)
