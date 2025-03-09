f8597dd678a73f08f8214d6e80420c7a4672559d
pranavbirajdar1 <pranavbi55@gmail.com>
2025-03-09 17:59:41 +0530

    using class mtd instead of celery

--- src/fundtransfer/views.py
+++ src/fundtransfer/views.py
@@ -2,14 +2,17 @@ from django.shortcuts import render , redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
-from .models import  Transaction
+from .models import  Transaction , TransactionLog
+from index.models import Contact
 from decimal import Decimal,InvalidOperation
 import logging
+from django.core.mail import send_mail
 # Create your views here.
 
 
 @login_required
 def fundtransfer(request):
+    senderEmail = Contact.objects.get(user=request.user)
     user = request.user
     if request.method == 'POST':
         receiver_usernames = request.POST.get('username')
@@ -53,11 +56,19 @@ def fundtransfer(request):
         # Perform the Transfer
         try:
             Transaction.transfer_funds(sender=sender,receiver=receiver,amount=amounts)
-            messages.success(request, f'Successfully Transferred {amounts} to {receiver_username.username}.')
+            messages.success(request, f'Successfully Transferred {amounts} to {receiver.username}.')
+            senderforlog =sender.username
+            receiverforlog = receiver.username
+            TransactionLog.objects.bulk_create(status='Successful',sender =senderforlog , receiver = receiverforlog)
+            send_sucessful_email(senderEmail,receiver_username,amounts)
             return redirect('fundtransfer')
             
         except Exception as e:
             messages.error(request, f'Error occurred during transfer {str(e)}')
+            senderforlog =sender.username
+            receiverforlog = receiver.username
+            TransactionLog.objects.bulk_create(status='Failed',sender =senderforlog , receiver = receiverforlog)
+            send_failure_email(senderEmail,receiver_username,amounts)
             return redirect('fundtransfer')
     # Fetch users excluding logged in user  (for dropdown option in template)
     users = User.objects.exclude(username=user.username).exclude(is_superuser=True)
@@ -66,3 +77,36 @@ def fundtransfer(request):
     }
     return render(request, 'fundtransfer.html',context=context)
 
+
+
+def send_failure_email(sender_email,receiver_username,amount):
+    #send email to sender for failed transaction
+    subject = f'Fund Transfer Failed'
+    messsage = (
+        f'Hello {sender_email},\n\n'
+        f'We have emailed inform you that Your Transfer of amount {amount} to {receiver_username} has failed due to technical errors,'
+        f'Please try again later or contact Support if the issue persists.\n\n'
+        f'Best regards,\n\n Your Banking Team'
+    )
+    send_mail(
+        subject=subject,
+        message=messsage,
+        from_email='your_email@gmail.com',
+        recipient_list=[sender_email],
+        fail_silently=False,
+    )
+def send_sucessful_email(sender_email,receiver_username,amount):
+    #send email to sender for failed transaction
+    subject = f'Fund Transfer Sucessful'
+    messsage = (
+        f'Hello {sender_email},\n\n'
+        f'We have emailed inform you that Your Transfer of amount {amount} to {receiver_username} has been Sucessful,'
+        f'Best regards,\n\n Your Banking Team'
+    )
+    send_mail(
+        subject=subject,
+        message=messsage,
+        from_email='your_email@gmail.com',
+        recipient_list=[sender_email],
+        fail_silently=False,
+    )
