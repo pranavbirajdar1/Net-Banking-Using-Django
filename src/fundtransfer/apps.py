2462744efdbf95891c85e4370bd9c733958be7d8
pranavbirajdar1 <pranavbi55@gmail.com>
2025-03-10 10:15:41 +0530

    Signal fr AccBAl Creation on User creation

--- src/fundtransfer/apps.py
+++ src/fundtransfer/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class FundtransferConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'fundtransfer'
+
+    def ready(self):
+        import fundtransfer.signals 
---
+++ src/fundtransfer/signals.py
@@ -0,0 +1,14 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from .models import Accbalance
+import logging 
+
+logger = getLogger(__name__)
+@receiver(post_save, sender=User)
+def log_user_login(sender, instance, created, **kwargs):
+    if created:
+        Accbalance.objects.create(user=instance)
+        
+        logger.info('USER BALANCE CREATED.')
+        print('USER BALANCE CREATED.')
