299f482da5f71b05e951893fc2018683f820d453
pranavbirajdar1 <pranavbi55@gmail.com>
2025-03-11 13:16:08 +0530

    Nominee Table

--- src/nomminee/models.py
+++ src/nomminee/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Nominee(models.Model):
+    id = models.BigAutoField(db_index=True, primary_key=True)
+    user = models.OneToOneField(User, verbose_name=("nominee"), on_delete=models.CASCADE , related_query_name='nominee')
+    name = models.CharField(max_length=10, verbose_name="First Name" , blank = False , null = False)
+    relation = models.CharField(max_length=18 , verbose_name='Relation' , blank=False , null= False)
+    contact = models.CharField(max_length=10, verbose_name="Contact Number" , blank = False , null = False)
+
+    def __str__(self):
+        return f'{self.user.username} Has Nominee : Name-> {self.name} : Relation-> {self.relation} : Contact-> {self.contact}'
+
+    def __unicode__(self):
+        return 
+    def save(self, *args, **kwargs):
+        
+        super(Nominee, self).save(*args, **kwargs)
