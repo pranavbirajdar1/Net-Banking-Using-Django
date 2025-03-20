7d6fdc687f60ca0f2cbf9bbf94507d5322b1aeee
pranavbirajdar1 <pranavbi55@gmail.com>
2025-03-20 11:12:25 +0530

    for account deatils too

--- src/fundtransfer/signals.py
+++ src/fundtransfer/signals.py
@@ -2,6 +2,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import Accbalance
+from index.models import AccountDetails
 import logging 
 
 logger = logging.getLogger(__name__)
@@ -9,6 +10,9 @@ logger = logging.getLogger(__name__)
 def log_user_login(sender, instance, created, **kwargs):
     if created:
         Accbalance.objects.create(user=instance)
+        AccountDetails.objects.create(user=instance)
         
         logger.info('USER BALANCE CREATED.')
+        logger.info('USER AccountDetails CREATED.')
         print('USER BALANCE CREATED.')
+        print('USER AccountDetails CREATED.')
